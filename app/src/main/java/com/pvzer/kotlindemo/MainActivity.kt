d1bc0f24c42af426c74dd6025cbcaf6c1d0fcea3
pvzercoffee <pvzercoffee@gmail.com>
2026-02-22 21:43:41 +0800

    article和task简单布局练习

--- app/src/main/java/com/pvzer/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/pvzer/kotlindemo/MainActivity.kt
@@ -4,27 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.pvzer.kotlindemo.demo.ComposeArticleDemo
+import com.pvzer.kotlindemo.demo.BirthdayCardDemo
+import com.pvzer.kotlindemo.demo.TaskCompletedDemo
 import com.pvzer.kotlindemo.ui.theme.KotlinDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,11 +31,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background,
                     ) {
 
-                    val happyBirthday = stringResource(R.string.happy_birthday);
-                    val targetName = stringResource(R.string.card_target_name);
-                    val pvzer = stringResource(R.string.pvzer);
-
-                    GreetingImage("$happyBirthday $targetName", pvzer)
+                    BirthyCardScreen();
+//                    ComposeArticleScreen()
+//                    TaskCompletedDemo()
 
                 }
             }
@@ -53,62 +42,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from:String, modifier: Modifier = Modifier){
+fun BirthyCardScreen(){
+    val happyBirthday = stringResource(R.string.happy_birthday);
+    val targetName = stringResource(R.string.card_target_name);
+    val pvzer = stringResource(R.string.pvzer);
 
-    Column(
-        modifier = modifier.padding(8.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        Text(
-            text = message,
-            modifier = Modifier,
-            fontSize = 70.sp,
-            lineHeight = 96.sp,
-            textAlign = TextAlign.Center,
-            color = Color.DarkGray
-        )
-
-        Text(
-            text = "from $from",
-            fontSize = 40.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
-
-    }
-
+    BirthdayCardDemo("$happyBirthday $targetName", pvzer)
 }
 
 @Composable
-fun GreetingImage(message: String, from : String,modifier: Modifier  = Modifier){
-    val image = painterResource(R.drawable.androidparty)
-
-    Box(modifier) {
-
-        Image(
-            painter = image,
-            contentDescription = "贺卡图片",
-            contentScale = ContentScale.Crop,
-            alpha = 0.5f
-        )
-        GreetingText(
-            message = message,
-            from =from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp),
-        )
-
-    }
-
-
+fun ComposeArticleScreen(){
+    ComposeArticleDemo(
+        image = painterResource(R.drawable.compose_article_head),
+        title = stringResource(R.string.compose_article_title),
+        abstract = stringResource(R.string.compose_article_abstract),
+        text = stringResource(R.string.compose_article_text),
+    )
 }
 
-@Preview
 @Composable
-fun greetingTextPreview(){
-    GreetingText(message = "Happy Birthday! Sam","pvzercoffee")
-    GreetingImage(message = "Happy Birthday! Sam","pvzercoffee")
+fun TaskCompletedScreen(){
+    TaskCompletedDemo()
 }
---
+++ app/src/main/java/com/pvzer/kotlindemo/demo/BirthyCard.kt
@@ -0,0 +1,71 @@
+package com.pvzer.kotlindemo.demo
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pvzer.kotlindemo.R
+
+@Composable
+private fun GreetingText(message: String, from:String, modifier: Modifier = Modifier){
+
+    Column(
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = message,
+            modifier = Modifier,
+            fontSize = 70.sp,
+            lineHeight = 96.sp,
+            textAlign = TextAlign.Center,
+            color = Color.DarkGray
+        )
+
+        Text(
+            text = "from $from",
+            fontSize = 40.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+
+    }
+
+}
+
+@Composable
+fun BirthdayCardDemo(message: String, from : String, modifier: Modifier  = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box(modifier) {
+
+        Image(
+            painter = image,
+            contentDescription = "贺卡图片",
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+        )
+
+    }
+}
